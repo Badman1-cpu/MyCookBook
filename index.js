74f056908a4cd2d048a4b00766944d6ad6cffa63
Tayler <taylerblevins@gmail.com>
2024-04-30 07:59:58 -0400

    js update

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('recipeForm').addEventListener('submit', function (event) {
-        event.preventDefault(); 
+//DOM Render Functions
+function renderOneRecipe(recipe){
+    //Bulid Reccipe Card
+    let card = document.createElement('li')
+    card.className = 'card'
+}
 
-        var recipeName = document.getElementById('recipeName').value;
-        var cookTime = document.getElementById('cookTime').value;
-        var description = document.getElementById('description').value;
 
-        var data = {
-            recipeName: recipeName,
-            cookTime: cookTime,
-            description: description
-        };
-        fetch('http://localhost:3000/recipes', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                })
-                .then(function (response) {
-                    if (response.ok) {
-                        console.log('Recipe added successfully!');
-                        // Redirect the user or show a success message here
-                    } else {
-                        console.error('Error adding recipe:', response.statusText);
-                        // Handle the error appropriately
-                    }
-                })
-                .catch(function (error) {
-                    console.error('Error adding recipe:', error);
-                    // Handle network errors or other unexpected issues
-                });
-            });
-        });
+
+
+//Initial Render
+function Initialize(){
+    recipeData.forEach(recipe => renderOneRecipe(recipe))
+}
