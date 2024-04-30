83ffb6dda50b698663ad1887131b0a47e2a16028
Tayler <taylerblevins@gmail.com>
2024-04-30 08:39:59 -0400

    js update

--- index.js
+++ index.js
@@ -15,9 +15,26 @@ function renderOneRecipe(recipe){
 }
 
 
-
+// Fetch JSON data from URL
+fetch('http://localhost:3000/recipes')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Define recipeData after fetching JSON data
+    const recipeData = data.recipes;
+    // Call Initialize function with fetched data
+    Initialize(recipeData);
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the recipe data:', error);
+  });
 
 //Initial Render
 function Initialize(){
     recipeData.forEach(recipe => renderOneRecipe(recipe))
 }
+Initialize()
